fb4f1d2e55fc15c86939693483da9db099ed932a
Jean-Luc19 <atticus19@gmail.com>
2017-06-16 10:24:12 -0600

    adds animation to sections on scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -58,10 +58,46 @@ $('a[href*="#"]')
     }
   });
 
+  // animations on scroll
 
+  $('.js--wp-1').waypoint(function() {
+    $('.js--wp-1').addClass('animated fadeIn');
+  },{
+    offset: '50%'
+  })
 
+  $('.js--wp-2').waypoint(function() {
+    $('.js--wp-2').addClass('animated fadeInUp');
+  },{
+    offset: '50%'
+  })
 
+  $('.js--wp-3').waypoint(function() {
+    $('.js--wp-3').addClass('animated fadeIn');
+  },{
+    offset: '50%'
+  })
 
+  $('.js--wp-4').waypoint(function() {
+    $('.js--wp-4').addClass('animated pulse');
+  },{
+    offset: '50%'
+  })
 
+  //Mobile Navi
 
+  $('.js--nav-icon').click(function() {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+
+    nav.slideToggle(200);
+
+    if (icon.hasClass('ion-navicon-round')) {
+      icon.removeClass('ion-navicon-round');
+      icon.addClass('ion-close-round');
+    } else {
+      icon.removeClass('ion-close-round');
+      icon.addClass('ion-navicon-round');
+    }
+  })
 });
